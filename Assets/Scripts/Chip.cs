ac41c0b94cada65debb843fd3e08546756010b22
Taimoor Rana <taimoorrana1@hotmail.com>
2016-09-04 12:49:20 -0400

    Chips are now placed on the board, in their appropriate location

--- Assets/Scripts/Chip.cs
+++ Assets/Scripts/Chip.cs
@@ -9,6 +9,8 @@ public class Chip : MonoBehaviour {
 	[SerializeField]
 	bool crown;
 	bool faceDown;
+	[SerializeField]
+	public int boardPosition;
 	private string ownerName = "";
 
 	// Use this for initialization
--- Assets/Scripts/PlayerBoard.cs
+++ Assets/Scripts/PlayerBoard.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerBoard : MonoBehaviour {
+	[SerializeField]
+	List<Transform> chipLocations;
 	List<Chip> chips = new List<Chip>();
 	// Use this for initialization
 	void Start () {
-	
+
+
 	}
 
 	// Update is called once per frame
@@ -16,8 +19,9 @@ public class PlayerBoard : MonoBehaviour {
 
 	public void addChip(Chip chip){
 		chips.Add (chip);
-		chip.gameObject.transform.position = gameObject.transform.position;
+		chip.gameObject.transform.position = chipLocations[chip.boardPosition].position;
 		string str = string.Concat("got ",chip.getColor()," ",chip.getName());
 		Debug.Log (str);
 	}
+		
 }
