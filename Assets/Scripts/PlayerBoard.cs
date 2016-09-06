9faf6243be0153e1ea1aa06571ec889b53cfef77
Taimoor Rana <taimoorrana1@hotmail.com>
2016-09-06 09:10:52 -0400

    working on UI

--- Assets/Scripts/PlayerBoard.cs
+++ Assets/Scripts/PlayerBoard.cs
@@ -6,6 +6,7 @@ public class PlayerBoard : MonoBehaviour {
 	[SerializeField]
 	List<Transform> chipLocations;
 	List<Chip> chips = new List<Chip>();
+	bool isVisible = false;
 	// Use this for initialization
 	void Start () {
 
@@ -19,6 +20,7 @@ public class PlayerBoard : MonoBehaviour {
 
 	public void addChip(Chip chip){
 		chips.Add (chip);
+		chip.gameObject.transform.parent = gameObject.transform;
 		chip.gameObject.transform.position = chipLocations[chip.boardPosition].position;
 		chip.gameObject.transform.rotation = chipLocations [chip.boardPosition].rotation;
 		chip.gameObject.transform.rotation *= Quaternion.Euler (-90, 0, 0);
@@ -27,4 +29,14 @@ public class PlayerBoard : MonoBehaviour {
 		Debug.Log (str);
 	}
 		
+	public void toggleView(){
+		isVisible = !isVisible;
+		gameObject.GetComponent<MeshRenderer> ().enabled = isVisible;
+
+		Renderer[] childrenRenderer = GetComponentsInChildren<Renderer> ();
+
+		foreach(Renderer trans in childrenRenderer){
+			trans.enabled = isVisible;
+		}
+	}
 }
